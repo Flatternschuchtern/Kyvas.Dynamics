0fc7f96fed35b177a6ab81acc2ccd2918e5b1a45
Kyrylo Vasko <kyvas@smart-it.com>
2020-07-28 16:49:23 +0300

    Removed unused

--- PluginBase.Standalone/Logic/StringValueAttributeReader.cs
+++ PluginBase.Standalone/Logic/StringValueAttributeReader.cs
@@ -8,31 +8,6 @@ namespace PluginBase.Standalone.Logic
 {
     public static class StringValueAttributeReader
     {
-        public static bool TryReadEnumValue<TEnum>(string input, out TEnum value)
-        where TEnum : struct
-        {
-            var values =
-                ((TEnum[])Enum.GetValues(typeof(TEnum)))
-                .SelectMany(
-                    f =>
-                    {
-                        var field = typeof(TEnum).GetField(f.ToString());
-                        var attributes = (StringValueAttribute[])field.GetCustomAttributes(typeof(StringValueAttribute), false);
-                        var pairs = new List<KeyValuePair<string, TEnum>>
-                        {
-                            new KeyValuePair<string, TEnum>(Enum.GetName(typeof(TEnum), f), f)
-                        };
-                        pairs
-                            .AddRange(
-                                attributes
-                                    .Select(a =>
-                                        new KeyValuePair<string, TEnum>(a.Value, f)));
-                        return pairs;
-                    })
-                .ToDictionary(p => p.Key, p => p.Value);
-            return values.TryGetValue(input, out value);
-        }
-
         public static string GetFirstValueOrName(PropertyInfo property)
         {
             var attributes = property.GetCustomAttributes(typeof(StringValueAttribute)).OfType<StringValueAttribute>();
